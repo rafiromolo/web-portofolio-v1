bbd27197003152099027020970235ef4f7de3e68
rafiromolo <rafiromolo@gmail.com>
2026-02-22 06:50:12 +0700

    add: about me section

---
+++ src/app/components/divider/index.tsx
@@ -0,0 +1,9 @@
+const Divider = () => {
+  return (
+    <div className="container">
+      <div className="h-4 border border-gray-200" />
+    </div>
+  )
+}
+
+export default Divider;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Home from "./pages/home";
+import Divider from "./components/divider";
 
 export default function Page() {
   return (
     <main>
       <Home />
+      <Divider />
     </main>
   );
 }
